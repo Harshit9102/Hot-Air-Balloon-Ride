243fb342049a1ddca62c725f5bcf03b79553691d
Harshit9102 <74136690+Harshit9102@users.noreply.github.com>
2021-03-27 18:32:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var position;
+
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1 = loadAnimation("Images/HotAirBallon-01.png");
+   balloonImage2 = loadAnimation("Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-03.png" )
+}
+
+function setup(){
+  database = firebase.database();
+  createCanvas(1500,700);
+
+  balloon = createSprite(250,650,150,150);
+  balloon.addAnimation("hotAirBallon",balloonImage2);
+  balloon.scale=0.5;
+
+ var balloonPosition = database.ref('balloon/position')
+  balloonPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+  background(bg);
+  
+if(keyDown(LEFT_ARROW)){
+  balloon.x = balloon.x-10
+}
+else if(keyDown(RIGHT_ARROW)){
+  balloon.x = balloon.x+10;
+}
+else if(keyDown(UP_ARROW)){
+  balloon.y = balloon.y-10;
+  balloon.scale = balloon.scale-0.005
+}
+else if(keyDown(DOWN_ARROW)){
+  balloon.y = balloon.y+10;
+  balloon.scale = balloon.scale+0.005
+}
+
+  drawSprites();
+  stroke("white");
+  textSize(30);
+  text("*Use Arrow Keys to Move the HotAirBalloon",40,40);
+}
+
+function updatePosition(x,y){
+  database.ref('balloon/position').set({
+    'x' : position.x + x,
+    'y' : position.y + y
+  })
+}
+
+function readPosition(data){
+  height = data.val();
+  balloon.x = position.x;
+  balloon.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
